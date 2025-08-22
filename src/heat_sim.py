2f64334fc4b68ed5ce43d7a12dd9c4b3c2f74a7b
hsumt <matthew.t.hsu@gmail.com>
2025-08-21 23:26:33 -0700

    Initialized 1D heat along a line using anim, grid setup and learned how a Finite Difference Method works

--- src/heat_sim.py
+++ src/heat_sim.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 N = 50 #points for the FDM
 L = 100 #rod length in inches
 h = L/(N+1) # spacing
@@ -13,10 +14,23 @@ temps = np.zeros(N+2)
 temps[:] = 25 + (np.random.random() *10) 
 temps[N//2] = 65
 
-new_temps = np.copy(temps)
-for i in range(1, N+1):
-    new_temps[i] = temps[i] + r *(temps[i-1] - 2*temps[i] + temps[i+1])
-temps = new_temps
+fig, ax = plt.subplots()
+line, = ax.plot(temps)
 
-plt.plot(temps)
+
+def frame_update(frame):
+    global temps
+    new_temps = np.copy(temps)
+    for i in range(1, N+1):
+        new_temps[i] = temps[i] + r *(temps[i-1] - 2*temps[i] + temps[i+1])
+        #Note to self: New temperature at i in the interior point range is the old temperature from before + some fraction of the difference between the neighbors. r is just the thermal diffusion controller
+    temps = new_temps
+    line.set_ydata(temps)
+    return line,
+
+    
+
+
+#plt.plot(temps)
+anim = FuncAnimation(fig, frame_update, frames=100, interval=50, blit=True)
 plt.show()
