c1fa4ec2f5405e9479cdcf1f0a70eedca15fa3a9
hsumt <matthew.t.hsu@gmail.com>
2025-08-21 23:03:14 -0700

    Initial Temperature Distribution

--- src/heat_sim.py
+++ src/heat_sim.py
@@ -2,5 +2,9 @@ import numpy as np
 N = 50 #points for the FDM
 L = 100 #rod length in inches
 h = L/(N+1) # spacing
-temp = np.zeros(N+2)
 
+
+temps = np.zeros(N+2)
+
+temps[:] = 25 + (np.random.random() *10) 
+temps[N//2] = 65
