5a1e18d02fe9c2ab9c9d5f16959cf51866cd2f1a
hsumt <matthew.t.hsu@gmail.com>
2025-08-21 23:10:36 -0700

    Graph generation

--- src/heat_sim.py
+++ src/heat_sim.py
@@ -1,10 +1,22 @@
 import numpy as np
+import matplotlib.pyplot as plt
 N = 50 #points for the FDM
 L = 100 #rod length in inches
 h = L/(N+1) # spacing
+alpha = 0.1 
+dt = 5
+r = (alpha*dt)/(h**2)
 
 
 temps = np.zeros(N+2)
 
 temps[:] = 25 + (np.random.random() *10) 
 temps[N//2] = 65
+
+new_temps = np.copy(temps)
+for i in range(1, N+1):
+    new_temps[i] = temps[i] + r *(temps[i-1] - 2*temps[i] + temps[i+1])
+temps = new_temps
+
+plt.plot(temps)
+plt.show()
